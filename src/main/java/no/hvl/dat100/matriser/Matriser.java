c741f3e422cd014d45497f9b557e8511ec81e8e6
h676664 <676664@stud.hvl.no>
2024-09-26 14:29:21 +0200

    Update Matriser.java

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -2,27 +2,66 @@ package no.hvl.dat100.matriser;
 
 public class Matriser {
 
+	public static void main(String[] args) {
+		int[][] matrise = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+		skrivUt(matrise);
+
+		String resultat = tilStreng(matrise);
+		System.out.println(resultat);
+
+		int[][] skalertMat = skaler(2, matrise);
+		for (int[] rad : skalertMat) {
+			for (int element : rad) {
+				System.out.print(element + " ");
+			}
+			System.out.println();
+		}
+	}
+
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+
+		for (int rad = 0; rad < matrise.length; rad++) {
+			for (int kol = 0; kol < matrise[rad].length; kol++) {
+				System.out.print(matrise[rad][kol] + " ");
+			}
+			System.out.println();
+			/*
+			 * for (int[] rad : matrise) { for (int element : rad) {
+			 * System.out.print(element + " "); } System.out.println();
+			 */
+		}
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
-		
+		StringBuilder streng = new StringBuilder();
+
+		for (int[] rad : matrise) {
+			for (int element : rad) {
+				streng.append(element).append(" ");
+			}
+			streng.append("\n");
+		}
+
+		return streng.toString();
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
+
+		int[][] nyMat = new int[matrise.length][matrise[0].length];
+
+		for (int rad = 0; rad < matrise.length; rad++) {
+			for (int kol = 0; kol < matrise[rad].length; kol++) {
+
+				nyMat[rad][kol] = matrise[rad][kol] * tall;
+			}
+		}
+
+		return nyMat;
 	}
 
 	// d)
@@ -30,16 +69,16 @@ public class Matriser {
 
 		// TODO
 		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
-		
+
 	}
-	
+
 	// e)
 	public static int[][] speile(int[][] matrise) {
 
 		// TODO
 
 		throw new UnsupportedOperationException("Metoden speile ikke implementert");
-	
+
 	}
 
 	// f)
@@ -47,6 +86,6 @@ public class Matriser {
 
 		// TODO
 		throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
-	
+
 	}
 }
